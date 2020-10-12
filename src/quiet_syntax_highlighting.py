4e24587f8466d0a97c06093aa03f37bf7b23623f
Seth Walker <sethrobertwalker@gmail.com>
2020-10-12 02:16:09 -0600

    Update quiet_syntax_highlighting.py

--- src/quiet_syntax_highlighting.py
+++ src/quiet_syntax_highlighting.py
@@ -81,7 +81,6 @@ class PythonSyntaxHighlight():
             data = self.text.get(row + ".0", row + "." + str(len(lines[int(row) - 1])))
 
             for token, content in lex(data, self.lexer()):
-                print(token, content)
                 self.text.mark_set("range_end", "range_start + %dc" % len(content))
                 self.text.tag_add(str(token), "range_start", "range_end")
                 self.text.mark_set("range_start", "range_end")
